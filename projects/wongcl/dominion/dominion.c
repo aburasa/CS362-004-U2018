c60584ee32c6b67be816f5831b6956d188b5ecc2
Clifford Wong <wongcl@oregonstate.edu>
2018-07-03 22:30:02 -0700

    Refactor Council Room card effect.

--- projects/wongcl/dominion/dominion.c
+++ projects/wongcl/dominion/dominion.c
@@ -82,6 +82,32 @@ int effectSteward(int handPos, int currentPlayer, int choice1, int choice2, int 
   return 0;
 }
 
+// Assignment 2 card effect function for Council Room
+int effectCouncilRoom(int handPos, int currentPlayer, struct gameState *state) {
+  //+4 Cards
+  for (int i = 0; i < 4; i++)
+  {
+    drawCard(currentPlayer, state);
+  }
+
+  //+1 Buy
+  state->numBuys++;
+
+  //Each other player draws a card
+  for (int i = 0; i < state->numPlayers; i++)
+  {
+    if ( i != currentPlayer )
+    {
+      drawCard(i, state);
+    }
+  }
+
+  //put played card in played card pile
+  discardCard(handPos, currentPlayer, state, 0);
+
+  return 0;	
+}
+
 int compare(const void* a, const void* b) {
   if (*(int*)a > *(int*)b)
     return 1;
@@ -744,28 +770,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return effectAdventurer(currentPlayer, state, temphand);
 			
     case council_room:
-      //+4 Cards
-      for (i = 0; i < 4; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-			
-      //+1 Buy
-      state->numBuys++;
-			
-      //Each other player draws a card
-      for (i = 0; i < state->numPlayers; i++)
-	{
-	  if ( i != currentPlayer )
-	    {
-	      drawCard(i, state);
-	    }
-	}
-			
-      //put played card in played card pile
-      discardCard(handPos, currentPlayer, state, 0);
-			
-      return 0;
+      return effectCouncilRoom(handPos, currentPlayer, state);
 			
     case feast:
       //gain card with cost up to 5
