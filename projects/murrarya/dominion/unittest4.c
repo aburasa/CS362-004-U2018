d81965e64fe79ff7006fa0ac9ea96e436fd2a377
Ryan Murray <Ryan.Murray@medhost.com>
2018-07-19 14:33:31 -0500

    Added unittest4.c for isGameOver()

---
+++ projects/murrarya/dominion/unittest4.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+
+#define VERBOSE 1
+
+enum bool{false, true};
+
+bool assertTrue(bool expr, char* description) {
+	printf("%s - ", description);
+	if (expr) {
+		printf("PASSED\n");
+	} else {
+		printf("FAILED\n")
+	}
+}
+
+void testIsGameOver() {
+	printf("\n--- TESTING isGameOver() ---\n");
+	
+	int seed = 1000;
+	int numPlayers = 2;
+	int maxBonus = 10;
+	struct gameState G;
+	int k[10] = {adventurer, council_room, feast, gardens, mine, remodel,
+				 smithy, village, baron, great_hall};
+
+	
+	memset(&G, 23, sizeof(struct gameState)); // clear gameState
+	initializeGame(numPlayers, k, seed, &G); // init a new game
+
+	int gameOver;
+	// Check that game doesn't end if a province card is left and no supply piles gone
+	gameOver = isGameOver(&G);
+	assertTrue(!gameOver, "game doesn't end if a province card is left and no supply piles gone");
+
+	// Game doesn't end if less than three supply piles are gone
+	G.supplyCount[smithy] = 0;
+	G.supplyCount[adventurer] = 0;
+	gameOver = isGameOver(&G);
+	assertTrue(!gameOver, "Game doesn't end if < 3 supply piles gone");
+	G.supplyCount[smithy] = 10;
+	G.supplyCount[adventurer] = 10;
+
+	// Check that game ends if all province cards are gone
+	G.supplyCount[province] = 0;
+	gameOver = isGameOver(&G);
+	assertTrue(gameOver, "Game ends if province cards are gone");
+	G.supplyCount[province] = 10;
+
+	// Check that game ends if three supply piles are gone
+	G.supplyCount[smithy] = 0;
+	G.supplyCount[adventurer] = 0;
+	G.supplyCount[feast] = 0;
+	gameOver = isGameOver(&G);
+	assertTrue(gameOver, "Game ends if three supply piles are gone");
+}	
